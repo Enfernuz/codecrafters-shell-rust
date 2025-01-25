3469963acac95c2246dedd98acc373b9b94619dc
Enfernuz <github.colog@aleeas.com>
2025-01-25 16:03:45 +0400

    Add echo command

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,23 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+        input = input.trim().to_string();
 
-        let command: &str = input.trim();
-        if "exit 0".eq(command) {
-            exit(0);
+        let parts: Vec<&str> = input.split_ascii_whitespace().collect();
+        let command: &str = parts[0];
+        let args: &[&str] = &parts.as_slice()[1..];
+
+        match command {
+            "exit" => {
+                if args.len() == 1 {
+                    let code: i32 = args[0]
+                        .parse()
+                        .expect("Code for exit must be an integer, e.g. exit 1");
+                    exit(code);
+                }
+            }
+            "echo" => println!("{}", args.join(" ")),
+            _ => println!("{command}: command not found"),
         }
-        println!("{command}: command not found");
     }
 }
